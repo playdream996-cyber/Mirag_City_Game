5032937498b161b59992d347ffdc33d8ee6a6e96
playdream996-cyber <playdream996@gmail.com>
2026-09-02 23:05:04 +0500

    Update build marker for physics-ground probe fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { PhysicsManager } from "./game/PhysicsManager";
 import { PlayerController } from "./game/PlayerController";
 import { buildWorld } from "./game/WorldBuilder";
 
-const BUILD_ID = "phase2-landingfix-2026-09-02-07";
+const BUILD_ID = "phase2-physicsprobe-2026-09-02-08";
 
 function supportLabel(state: CharacterSupportedState): string {
   switch (state) {
